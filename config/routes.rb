1def3cddce551f3f0183923f92ad88b84d26c612
juohmaru94 <dimmoe@hotmail.com>
2023-02-05 14:35:29 +0200

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'public#index'
+  # get 'home/index'
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
